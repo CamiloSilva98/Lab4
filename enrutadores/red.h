05155be0c86ea708507c0178540e0f7e9e2ecd4f
CamiloSilva98 <Brayan.silva@udea.edu.co>
2024-10-20 17:03:54 -0500

    .

---
+++ enrutadores/red.h
@@ -0,0 +1,12 @@
+#ifndef RED_H
+#define RED_H
+
+#include <QObject>
+
+class Red
+{
+public:
+    Red();
+};
+
+#endif // RED_H
---
+++ enrutadores/router.h
@@ -0,0 +1,12 @@
+#ifndef ROUTER_H
+#define ROUTER_H
+
+#include <QObject>
+
+class Router
+{
+public:
+    Router();
+};
+
+#endif // ROUTER_H
