cd508af38a084630f8cbf84f55571583c2681988
CamiloSilva98 <Brayan.silva@udea.edu.co>
2024-10-22 14:10:36 -0500

    .

--- enrutadores/main.cpp
+++ enrutadores/main.cpp
@@ -1,9 +1,22 @@
 #include "red.h"
-
+#include <iostream>
+using namespace std;
 int main()
 {
     Red red;
-
+    int op;
+    do
+    {
+        cout << "\n--- Menu Principal ---\n";
+        cout << "1. Agregar router.\n";
+        cout << "2. Eliminar router.\n";
+        cout << "3. Actualizar tabla.\n";
+        cout << "4. Cargar tabla.\n";
+        cout << "0. Guardar y Salir\n";
+        cout << "Seleccione una opcion: ";
+        cin >> op;
+    }
+    while(op != 0);
     // Agregar enrutadores
     red.addRouter("A");
     red.addRouter("B");
